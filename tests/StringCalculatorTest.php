1fb27deae0215d75599dc62e4221c3a95fb0e858
Jon González Garrido <bizkaitarra@gmail.com>
2022-09-14 16:00:22 +0200

    Initial configuration for StringCalculator kata

---
+++ tests/StringCalculatorTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Tests;
+
+use App\StringCalculator;
+use PHPUnit\Framework\TestCase;
+
+class StringCalculatorTest extends TestCase
+{
+    public function testEmptyStringShouldReturnZero() {
+        $stringCalculator = new StringCalculator();
+        $this->assertEquals(0,$stringCalculator->add(""));
+    }
+
+}

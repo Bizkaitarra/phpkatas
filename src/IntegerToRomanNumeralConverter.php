046e36d3a1888773ccaa75e8d9944a4fdd3a787a
Jon González Garrido <bizkaitarra@gmail.com>
2022-09-19 10:53:17 +0200

    Refactor using native PHP function for more simple solution

--- src/IntegerToRomanNumeralConverter.php
+++ src/IntegerToRomanNumeralConverter.php
@@ -7,8 +7,7 @@ class IntegerToRomanNumeralConverter
     public function __invoke(int $number):string
     {
         if ($number < 4) {
-            $solution = str_repeat('I', $number);
-            return $solution;
+            return str_repeat('I', $number);;
         }
         return 'IV';
 
